652a66576b7a5d488248bbbbbe6f71875334a083
Vid Dima <aseg55466@gmail.com>
2025-03-31 17:33:50 +0500

    Бондарев3ПК2

---
+++ ConsoleApp2/Cocktail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Cocktail : Beverage
+    {
+        protected override void Brew()
+        {
+            Console.WriteLine("Смешивание ингредиентов коктейля");
+        }
+
+        protected override void AddCondiments()
+        {
+            Console.WriteLine("Добавляем лёд и украшения");
+        }
+    }
+}
