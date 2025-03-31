490a5e9204d3a7ddf0e843d1bc5bf1c953dc1d5e
Дарья Павловна Ахметова <akhmetova.dp2204@oksei.onmicrosoft.com>
2025-03-31 17:04:13 +0500

    Ахметова Дарья

---
+++ ConsoleApp2/Cocktail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Cocktail :Beverage
+    {
+        protected override void Brew()
+        {
+            Console.WriteLine("Смешивание ингридиентов коктейля");
+        }
+
+        protected override void AddCondiments()
+        {
+            Console.WriteLine("Добавляем трубочку");
+        }
+    }
+}
